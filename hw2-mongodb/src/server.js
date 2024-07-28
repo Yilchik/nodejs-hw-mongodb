0a16724b723ed6a7a337c94737a9ca4dac128071
Yilchik <yulyayulia2@gmail.com>
2024-07-28 15:02:56 +0200

    end

--- hw2-mongodb/src/server.js
+++ hw2-mongodb/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
+import { getAllContacts, getContactsById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -40,5 +41,31 @@ export const setupServer = () => {
 
   app.listen(PORT, () => {
     console.log('Server is running on ${PORT}');
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactsById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      message: 'Successfully found contact with id ${contactId}!',
+      data: contact,
+    });
   });
 };
